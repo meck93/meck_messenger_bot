a84502fd240fc29a3e534b60bd952074ec1df6da
Moritz Eck <moritz.eck@gmail.com>
2018-12-15 17:26:24 +0100

    added more documented version

--- src/server.js
+++ src/server.js
@@ -24,19 +24,36 @@ app.get("/", (req, res) => {
   res.send("Hello world, I am a chat bot for facebook messenger.");
 });
 
-// for facebook verification
-app.get("/webhook/", (req, res) => {
-  if (req.query["hub.verify_token"] === process.env.FB_URL_TOKEN) {
-    res.send(req.query["hub.challenge"]);
+// Adds support for GET requests to our webhook
+app.get("/webhook", (req, res) => {
+  // Your verify token. Should be a random string.
+  let VERIFY_TOKEN = process.env.FB_URL_TOKEN;
+
+  // Parse the query params
+  let mode = req.query["hub.mode"];
+  let token = req.query["hub.verify_token"];
+  let challenge = req.query["hub.challenge"];
+
+  // Checks if a token and mode is in the query string of the request
+  if (mode && token) {
+    // Checks the mode and token sent is correct
+    if (mode === "subscribe" && token === VERIFY_TOKEN) {
+      // Responds with the challenge token from the request
+      console.log("WEBHOOK_VERIFIED");
+      res.status(200).send(challenge);
+    } else {
+      // Responds with '403 Forbidden' if verify tokens do not match
+      res.sendStatus(403);
+    }
   }
-  res.send("Error, wrong token");
 });
 
 function sendTextMessage(sender, text) {
   let messageData = { text: text };
+
   request(
     {
-      url: "https://graph.facebook.com/v2.6/me/messages",
+      url: "https://graph.facebook.com/v3.2/me/messages",
       qs: { access_token: process.env.FB_ACCESS_TOKEN },
       method: "POST",
       json: {
@@ -44,6 +61,7 @@ function sendTextMessage(sender, text) {
         message: messageData
       }
     },
+
     function(error, response, body) {
       if (error) {
         console.log("Error sending messages: ", error);
@@ -54,17 +72,36 @@ function sendTextMessage(sender, text) {
   );
 }
 
-app.post("/webhook/", (req, res) => {
-  let messaging_events = req.body.entry[0].messaging;
-  for (let i = 0; i < messaging_events.length; i++) {
-    let event = req.body.entry[0].messaging[i];
-    let sender = event.sender.id;
-    if (event.message && event.message.text) {
-      let text = event.message.text;
-      sendTextMessage(sender, "Text received, echo: " + text.substring(0, 200));
-    }
+// Creates the endpoint for our webhook
+app.post("/webhook", (req, res) => {
+  let body = req.body;
+
+  // Checks this is an event from a page subscription
+  if (body.object === "page") {
+    // Iterates over each entry - there may be multiple if batched
+    body.entry.forEach(function(entry) {
+      // Gets the message. entry.messaging is an array, but
+      // will only ever contain one message, so we get index 0
+      let webhook_event = entry.messaging[0];
+      console.log(webhook_event);
+
+      let sender = webhook_event.sender.id;
+
+      if (webhook_event.message && webhook_event.message.text) {
+        let text = webhook_event.message.text;
+        sendTextMessage(
+          sender,
+          "Text received, echo: " + text.substring(0, 200)
+        );
+      }
+    });
+
+    // Returns a '200 OK' response to all requests
+    res.status(200).send("EVENT_RECEIVED");
+  } else {
+    // Returns a '404 Not Found' if event is not from a page subscription
+    res.sendStatus(404);
   }
-  res.sendStatus(200);
 });
 
 // run server and listen for incoming requests
